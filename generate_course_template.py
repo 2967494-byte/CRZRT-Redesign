14d674b27d7f5e25cd46641e9d2a9379e5aa83b9
Matvey <2967494@gmail.com>
2026-07-16 16:23:39 +0300

    feat: update course template structure and automate its generation process

--- generate_course_template.py
+++ generate_course_template.py
@@ -74,7 +74,7 @@ course_content = """
           </div>
           <div class="course-widget-item">
             <div class="course-widget-item__icon">
-              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
+              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M8 6v12 M8 6h5.5a3 3 0 0 1 0 6H8 M6 12h6 M6 15h6"></path></svg>
             </div>
             <div class="course-widget-item__text">
               <span class="course-widget-item__label">Стоимость</span>
